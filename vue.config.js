6a3210e71722578ef1204f2d105dc8bab995af6d
PaulGuerry <paul.guerry@gmail.com>
2025-03-05 17:18:56 +0100

    Correct errors in package.json and vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,8 @@
 
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  publicPath: '/',
-||||||| c62896b7
-  publicPath: '/',
-=======
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/pytheas_devtest/'
+    ? '/pytheas-db/'
     : '/',
->>>>>>> ddd44671570fbe9ec745f9aa2f0f6db7a89c9468
   configureWebpack: {
         module: {
             rules: [ // proper handle of .mjs files
@@ -21,10 +14,5 @@ module.exports = {
             ]
         }
     }
-=======
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/13_devtest/'
-    : '/'
->>>>>>> e5809672622a4ac99f3f3368870f01e26ccbcb86
 }
 
