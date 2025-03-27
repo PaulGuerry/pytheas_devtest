6d5958efb6919be4e778e570e3f1b564afa7901f
PaulGuerry <paul.guerry@gmail.com>
2025-03-27 15:45:44 +0100

    Update AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template lang="html">
-    <div>
-       <p>AboutView page.</p> 
+    <div class="w-full grid grid-cols-1 place-items-center my-10 mx-auto">
+        <img class="w-[100px] h-auto md:w-[200px] lg:w-[300px] " src="@/assets/PYTHEAS_Logo.svg" >
+        <p  class="text-xl  text-gray-800 my-20"> If you use PytheasDB, please cite <a href="https://doi.org/10.5582/irdr.2024.01043"> Percheron et al. <span class="italic"> Intractable Rare Dis Res </span> (2024) </a> </p>
+        <p  class="text-xl  text-gray-800 my-20"> Please send all comments/queries/requests to <span class="font-bold"> contact.pytheasdb@gmail.com </span> </p>
     </div>
 </template>
 <script>
