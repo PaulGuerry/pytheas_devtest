de15ed4e4eb06df63aa8f0462fde365f5e5cb7d6
PaulGuerry <paul.guerry@gmail.com>
2025-07-07 15:32:55 +0200

    attempt again with gtag.js

--- src/components/headers/MainHeader.vue
+++ src/components/headers/MainHeader.vue
@@ -1,5 +1,14 @@
 <template lang="html">
     <header class="hidden lg:block">
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z1TQ8HNHV1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Z1TQ8HNHV1');
+        </script>
         <nav class="flex items-center justify-between py-5 px-10">
             <router-link to="/">
                 <img class="w-[50px] h-auto md:w-[100px] lg:w-[200px] " src="@/assets/PYTHEAS_Logo.svg">
@@ -38,6 +47,8 @@
                 </label>
             </div>
         </nav>
+    </header>
+    <header class="block lg:hidden">
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z1TQ8HNHV1"></script>
         <script>
@@ -47,8 +58,6 @@
 
           gtag('config', 'G-Z1TQ8HNHV1');
         </script>
-    </header>
-    <header class="block lg:hidden">
         <nav class="flex items-center justify-between py-5 px-10">
             <router-link to="/">
                 <img class="w-[50px] h-auto md:w-[100px] lg:w-[200px] " src="@/assets/PYTHEAS_Logo.svg">
@@ -66,15 +75,6 @@
                 </router-link>
             </div>
         </nav>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z1TQ8HNHV1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-Z1TQ8HNHV1');
-    </script>
     </header>
 </template>
 <script>
