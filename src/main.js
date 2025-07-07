c8dba476c9cf8584b072788d7e5cbcb203b655f3
PaulGuerry <paul.guerry@gmail.com>
2025-07-07 15:58:10 +0200

    gtag createGtag

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { configure } from "vue-gtag"
+import { createGtag } from "vue-gtag"
 import './style.css'
 
 // import rubik font using fontsource
@@ -24,8 +24,14 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
+
+// gtag
+const gtag = createGtag({
+  tagId: "G-Z1TQ8HNHV1"
+})
+
 /* add icons to the library */
 library.add(faUserSecret, faUser, faCartShopping, faSearch)
 
-createApp(App).use(store).use(router).use((configure, { tagId : "G-Z1TQ8HNHV1" } )).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(store).use(router).use(gtag).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 //createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
